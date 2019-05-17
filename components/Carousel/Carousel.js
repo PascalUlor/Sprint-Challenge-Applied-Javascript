125f499a8f8331fd8a4f615d666118150f7a5b25
PascalUlor <pascalulor@yahoo.com>
2019-05-17 13:59:21 +0100

    ft(finish up carousel) add animation for carousel style carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -4,24 +4,31 @@ class Carousel {
         this.images = this.carousel.querySelectorAll('img');
         this.rightButton = this.carousel.querySelector('.right-button');
         this.leftButton = this.carousel.querySelector('.left-button');
-        this.slide();
+
+        this.leftButton.addEventListener('click', () => {
+            this.move(false)
+            TweenMax.to(".left", 2, {ease:Elastic.easeOut});
+        });
+        this.rightButton.addEventListener('click', () =>{
+            this.move(true)
+            TweenMax.to(".right", 2, {ease:Elastic.easeOut});
+        });
       }
 
-      slide() {
-          if (this.rightButton) {
-            this.rightButton.addEventListener('click', ()=>{
-                this.images.forEach(image => {
-                  image.setAttribute('style', 'transform:translateX(-' + 100 + '%)');
-                });
-            })
-          } else if (this.leftButton) {
-            this.leftButton.addEventListener('click', ()=>{
-                this.images.forEach(image => {
-                  image.setAttribute('style', 'transform:translateX(+' + 100 + '%)');
-                });
-            })
+      move(right) {
+        const current = this.carousel.querySelector('.active');
+        this.data = current.dataset.img;
+        const num = right ? 1 : -1;
+        const nextImg = this.carousel.querySelector(`.img[data-img="${Number(this.data) + num}"]`);
+        if (nextImg) {
+          if (right) {
+            current.classList.replace('active', 'left');
+          } else {
+            current.classList.replace('active', 'right');
           }
-          
+          nextImg.classList.remove('left', 'right');
+          nextImg.classList.add('active');
+        }
       }
       
     
