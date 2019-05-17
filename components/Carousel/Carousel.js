e2ac2fae76274821f619653b9eed879dcc27247f
PascalUlor <pascalulor@yahoo.com>
2019-05-17 10:08:53 +0100

    ft(set carousel img to display block and select them frm the dom)

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,13 @@
 class Carousel {
-
+    constructor(carousel) {
+        this.carousel = carousel;
+        this.images = this.carousel.querySelectorAll('img');
+      }
+    
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
