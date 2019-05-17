b77a1c012a95c73898a0fd22291a0e55c2e93ed3
PascalUlor <pascalulor@yahoo.com>
2019-05-17 11:09:35 +0100

    ft(create slide function)

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,12 +2,34 @@ class Carousel {
     constructor(carousel) {
         this.carousel = carousel;
         this.images = this.carousel.querySelectorAll('img');
+        this.rightButton = this.carousel.querySelector('.right-button');
+        this.leftButton = this.carousel.querySelector('.left-button');
+        this.slide();
       }
+
+      slide() {
+          if (this.rightButton) {
+            this.rightButton.addEventListener('click', ()=>{
+                this.images.forEach(image => {
+                  image.setAttribute('style', 'transform:translateX(-' + 100 + '%)');
+                });
+            })
+          } else if (this.leftButton) {
+            this.leftButton.addEventListener('click', ()=>{
+                this.images.forEach(image => {
+                  image.setAttribute('style', 'transform:translateX(+' + 100 + '%)');
+                });
+            })
+          }
+          
+      }
+      
     
 }
 
 let carousel = document.querySelector('.carousel');
-new Carousel(carousel);
+let slide = new Carousel(carousel);
+console.log(slide);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
